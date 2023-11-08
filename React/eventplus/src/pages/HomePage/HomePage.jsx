fabd1b410475fa6dab2c98f0421293ffb1f54fa4
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-11-08 10:50:37 -0300

    instalado o axios

--- React/eventplus/src/pages/HomePage/HomePage.jsx
+++ React/eventplus/src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import "./HomePage.css";
 import Banner from "../../componentes/Banner/Banner";
+import Container from "../../componentes/Container/Container";
+import NextEvent from "../../componentes/NextEvent/NextEvent";
+import ContactSection from "../../componentes/ContactSection/ContactSection";
 import MainContent from "../../componentes/MainContent/MainContent";
 import VisionSection from "../../componentes/VisionSection/VisionSection";
-import "./HomePage.css";
+import Titulo from "../../componentes/Titulo/Titulo";
 
 const HomePage = () => {
   return (
-    <div>
-      <MainContent>
-        <Banner />
-        <VisionSection />
-      </MainContent>
-    </div>
+    <MainContent>
+      <Banner />
+
+      <section className="proximos-eventos">
+        <Container>
+          <Titulo titleText={"Próximos Eventos"} />
+
+          <div className="events-box">
+            <NextEvent 
+              title={"Evento X"}
+              description={"Evento Bacana"}
+              eventDate={"10/11/2023"}
+              idEvent={"jjiedeidjeduied"}
+              />
+          </div>
+        </Container>
+      </section>
+
+      <VisionSection />
+      <ContactSection />
+    </MainContent>
   );
 };
 
